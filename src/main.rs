19f48963ec32fbc3c0a52bd372766fca93bf266d
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-05-31 11:11:03 +0800

    extract lib.rs to remove shit warning

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,17 @@
 use from_pest::FromPest;
-use parser::{EFile, Rule, TreeParser};
 use pest::Parser;
 use std::fs;
-
-mod parser;
+use tr::parser::{EFile, Rule, TreeParser};
 
 fn main() {
     let input = fs::read_to_string("trees/xxx-0001.tree").unwrap();
     let mut output = TreeParser::parse(Rule::file, input.as_str()).unwrap();
     match EFile::from_pest(&mut output) {
-        Ok(f) => println!("{:?}", f),
+        Ok(f) => {
+            for t in f.tops {
+                println!("{:?}", t)
+            }
+        }
         Err(err) => panic!("{}", err),
     }
 }
