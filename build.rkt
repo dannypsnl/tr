284faae5a7116c7face2d453f9399ca6bd876500
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-05-31 17:03:24 +0800

    generate basic index.html (without TOC, related, backlinks, etc)

--- build.rkt
+++ build.rkt
@@ -9,13 +9,20 @@
     (build-path output-dir "embed.html")
     #:exists 'replace))
 
-  (process*/ports f (current-input-port) (current-output-port) (find-executable-path "racket") (path-add-extension addr ".scrbl")))
+  (process*/ports f (current-input-port) (current-output-port) (find-executable-path "racket") (path-add-extension addr ".scrbl"))
+
+  (printf "embed ~s\n" addr))
 
 (define (build-index addr)
   (define output-dir (build-path "output/" addr))
   (build-path output-dir "embed.html")
 
-  
+  (define f (open-output-file
+    (build-path output-dir "index.html")
+    #:exists 'replace))
+  (process*/ports f (current-input-port) (current-output-port) (find-executable-path "racket") (path-add-extension addr ".scrbl"))
+
+  (printf "index ~s\n" addr)
   )
 
 (define addr-list
--- tr.rkt
+++ tr.rkt
@@ -6,9 +6,8 @@
 
 (define (tr-title text)
   (define current-scrbl-path (find-system-path 'run-file))
-  (define self-url (path-replace-extension current-scrbl-path ".html"))
   (define id (path->string (path-replace-extension current-scrbl-path "")))
-  (define link-self (a 'href: (path->string self-url) 'target: "_parent" "[" id "]"))
+  (define link-self (a 'href: (string-append "/" id) 'target: "_parent" "[" id "]"))
   (h2 text " " link-self))
 
 (define toc-list (mutable-set))
@@ -18,12 +17,11 @@
     (ul
       (for/list ([entry (set->list toc-list)]) (li entry)))))
 
-
 (define (tree #:title title-text #:taxon [taxon #f] . content)
   (html
    (head
     (title title-text)
-    (link 'rel: "stylesheet" 'href: "style.css")
+    (link 'rel: "stylesheet" 'href: "/style.css")
     )
    (body
     (article
@@ -39,4 +37,4 @@
   ; output
   (details 'open: "open"
     "<summary>" address "</summary>"
-    (embed 'type: "text/html" 'src: (string-append address ".html"))))
+    (embed 'type: "text/html" 'src: (string-append "/" address "/embed.html"))))
