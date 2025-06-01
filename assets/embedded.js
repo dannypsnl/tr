940454b157064976822ae2bb806557a70acd539a
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-06-01 15:21:50 +0800

    no need to adjust width, add a bit (20px) more px for height

--- assets/embedded.js
+++ assets/embedded.js
@@ -2,8 +2,7 @@ const frames = document.getElementsByClassName("embedded");
 for (const frame of frames) {
   frame.onload = function () {
     // set the height of the iframe as the height of the iframe content
-    frame.style.height = frame.contentWindow.document.body.scrollHeight + "px";
-    // set the width of the iframe as the width of the iframe content
-    frame.style.width = frame.contentWindow.document.body.scrollWidth + "px";
+    frame.style.height =
+      frame.contentWindow.document.body.scrollHeight + 20 + "px";
   };
 }
