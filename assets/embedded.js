33dd733ac11810793d0aa7a07e677e169de149eb
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-06-04 02:00:40 +0800

    (css) improve embed card result
    * remove width require of .center
    * add more height for embed page

--- assets/embedded.js
+++ assets/embedded.js
@@ -3,6 +3,6 @@ for (const frame of frames) {
   frame.onload = function () {
     // set the height of the iframe as the height of the iframe content
     frame.style.height =
-      frame.contentWindow.document.body.scrollHeight + 20 + "px";
+      frame.contentWindow.document.body.scrollHeight + 30 + "px";
   };
 }
