4321486560e3c34cfb31802fc62101569802c544
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2025-07-05 14:33:46 +0800

    stack command-line
    resolve #42

--- command.rkt
+++ command.rkt
@@ -6,6 +6,7 @@
          "private/common.rkt"
          "build.rkt"
          "metadata.rkt")
+(require racket/logging)
 
 (define (find-root-dir dir)
   (if (string=? "/" (path->string dir))
@@ -25,76 +26,120 @@
             (copy-directory-recursively source-path target-path)
             (copy-file source-path target-path #t))))))
 
-(module+ main
-  (define args
-    (let ([dst-dir (make-parameter "_build")])
-      (command-line
-       #:once-each
-       ["--dest" file
-                 "destination directory of build, default is `_build`"
-                 (dst-dir (string->path file))]
-       #:usage-help
-       "tr command"
-       #:args args
-       args)))
+(define (run-tr-init)
+  (command-line
+    #:program "tr init"
+    #:usage-help "setup tr-notes project for beginner"
+    #:args _
+    (call-with-output-file ".gitignore"
+      (λ (out)
+        (displayln "_tmp/" out)
+        (displayln "_build/" out)
+        (displayln "assets/" out)))
+    (call-with-output-file "site.json"
+      (λ (out)
+        (define x (make-hasheq
+          '((domain . "your domain")
+            (title . "your site title")
+            (description . "your site description"))))
+        (write-json x out)))
+    (system* (find-executable-path "git") "clone" "https://git.sr.ht/~dannypsnl/tr-assets" "assets")
+    (system* (find-executable-path "git") "init")
+    (make-directory* "content/post")
+    (displayln "init done")))
 
-  (define root-path (find-root-dir (current-directory)))
-  (parameterize ([current-directory (if root-path root-path (current-directory))])
-    (match args
-      [(list "init")
-        (call-with-output-file ".gitignore"
-          (λ (out)
-            (displayln "_tmp/" out)
-            (displayln "_build/" out)
-            (displayln "assets/" out)))
-        (call-with-output-file "site.json"
-          (λ (out)
-            (define x (make-hasheq
-              '((domain . "your domain")
-                (title . "your site title")
-                (description . "your site description"))))
-            (write-json x out)))
-        (system* (find-executable-path "git") "clone" "https://git.sr.ht/~dannypsnl/tr-assets" "assets")
-        (system* (find-executable-path "git") "init")
-        (make-directory* "content/post")
-        (displayln "init done")]
-      [(list "watch")
-        (unless root-path (raise "You're not in a tr project"))
-        (define scrbl-list (find-files (lambda (x) (path-has-extension? x #".scrbl")) "content"))
-        (thread-wait
-          (watch scrbl-list
-            (λ (_paths)
-              (search-and-build "content"))
-            (λ (_)
-              (void))))]
-      [(list "build")
-        (unless root-path (raise "You're not in a tr project"))
-        (define site-obj (file->json "site.json"))
-        ; If user didn't assign one, use our setup
-        (define assets-directories (hash-ref site-obj 'assets '("assets")))
-        (for ([path assets-directories])
-          (copy-directory-recursively path "_build"))
-        (search-and-build "content")]
-      [(list "next" addr-prefix)
-        (unless root-path (raise "You're not in a tr project"))
-        (define scrbl-list
-          (find-files
-            (λ (path)
-              (and (path-has-extension? path #".scrbl")
-                   (string-prefix? (basename path) addr-prefix)))
-            "content"))
-        (define numbers
-          (for/list ([path scrbl-list])
-            (define b (basename (path-replace-extension path "")))
-            (define number-text (string-trim b (string-append addr-prefix "-") #:right? #f))
-            (define n (base36->int number-text))
-            ; if not a number, we use 0 as value
-            (if n n 0)))
-        (define max-num (apply max (cons -1 numbers)))
-        (displayln (string-append addr-prefix "-" (int->base36 (add1 max-num))))]
-      [(list "meta" addr)
-        (define scrbl-list (find-files (lambda (x) (string=? addr (path->string (path-replace-extension (basename x) "")))) "content"))
-        (write-json (compute-metadata addr (first scrbl-list)))]
-      [(list cmd _ ...) (printf "Unknown command `~a`~n" cmd)]
-      [_ (displayln "No command provided")]
-      )))
+(define root-path (find-root-dir (current-directory)))
+(define (run-tr-build)
+  (command-line
+    #:program "tr build"
+    #:usage-help "build tr-notes project"
+    #:args _
+    (unless root-path (raise "You're not in a tr project"))
+      (define site-obj (file->json "site.json"))
+    ; If user didn't assign one, use our setup
+    (define assets-directories (hash-ref site-obj 'assets '("assets")))
+    (for ([path assets-directories])
+      (copy-directory-recursively path "_build"))
+    (search-and-build "content")))
+(define (run-tr-watch)
+  (command-line
+    #:program "tr watch"
+    #:usage-help "watch and rebuild tr-notes project"
+    #:args _
+    (unless root-path (raise "You're not in a tr project"))
+    (define scrbl-list (find-files (lambda (x) (path-has-extension? x #".scrbl")) "content"))
+    (thread-wait
+      (watch scrbl-list
+        (λ (_paths)
+          (search-and-build "content"))
+        (λ (_)
+          (void))))))
+
+(define (run-tr-next)
+  (command-line
+    #:program "tr next"
+    #:usage-help "compute next address for <prefix>"
+    #:args (prefix)
+    (unless root-path (raise "You're not in a tr project"))
+    
+    (define scrbl-list
+      (find-files
+        (λ (path)
+          (and (path-has-extension? path #".scrbl")
+               (string-prefix? (basename path) prefix)))
+        "content"))
+    (define numbers
+      (for/list ([path scrbl-list])
+        (define b (basename (path-replace-extension path "")))
+        (define number-text (string-trim b (string-append prefix "-") #:right? #f))
+        (define n (base36->int number-text))
+        ; if not a number, we use 0 as value
+        (if n n 0)))
+    (define max-num (apply max (cons -1 numbers)))
+    (displayln (string-append prefix "-" (int->base36 (add1 max-num))))))
+
+(define (run-tr-meta)
+  (command-line
+    #:program "tr meta"
+    #:usage-help "get metadata of <addr>"
+    #:args (addr)
+    (unless root-path (raise "You're not in a tr project"))
+    (define scrbl-list (find-files (lambda (x) (string=? addr (path->string (path-replace-extension (basename x) "")))) "content"))
+    (write-json (compute-metadata addr (first scrbl-list)))))
+
+(define (run-tr)
+  (command-line
+   #:program "tr"
+
+   #:usage-help
+   "\n<command> is one of
+
+\tinit
+\tbuild
+\twatch
+\tnext
+\tmeta
+
+For help on these, use 'build --help', 'next --help', etc."
+
+   #:ps "\nSee https://tr-notes.srht.site/guide-0005 for details."
+   #:args (command . leftover-args)
+   (define leftover-arg-vector (vector->immutable-vector (list->vector leftover-args)))
+   (define (call-command command-thunk)
+     (parameterize ([current-command-line-arguments leftover-arg-vector])
+       (with-logging-to-port (current-error-port)
+         command-thunk
+         #:logger (current-logger)
+         'info 'tr
+         'error)))
+
+   (parameterize ([current-directory (if root-path root-path (current-directory))])
+     (match command
+       ["init" (call-command run-tr-init)]
+       ["build" (call-command run-tr-build)]
+       ["watch" (call-command run-tr-watch)]
+       ["next" (call-command run-tr-next)]
+       ["meta" (call-command run-tr-meta)]))))
+
+(module+ main
+  (run-tr))
