4a969b798095acac0bc33f6bb223586f6a71cc67
Lîm Tsú-thuàn <inbox@dannypsnl.me>
2026-01-07 01:06:24 +0800

    support fediverse site validation and signature
    Insert entry
    
      "fedi": {
        "site": "your fediverse site",
        "handle": "your fediverse id"
      }
    
    in `site.json` to do so

--- generate-index.rkt
+++ generate-index.rkt
@@ -6,13 +6,13 @@
          scribble/html/xml
          "private/config.rkt"
          (only-in "card.rkt"
-            self-addr toc/depth
-            tree
-            generate-toc
-            generate-context
-            generate-references
-            generate-backlinks
-            generate-related))
+                  self-addr toc/depth
+                  tree
+                  generate-toc
+                  generate-context
+                  generate-references
+                  generate-backlinks
+                  generate-related))
 
 (define (root? addr)
   (string=? addr "index"))
@@ -25,55 +25,72 @@
     (make-directory* output-dir)
     (define out
       (open-output-file #:exists 'truncate/replace
-        (if (root? addr)
-          (build-path (get-output-path) "index.html")
-          (build-path (get-output-path) addr "index.html"))))
+                        (if (root? addr)
+                            (build-path (get-output-path) "index.html")
+                            (build-path (get-output-path) addr "index.html"))))
 
     (define metaobj (hash-ref addr-maps-to-metajson addr))
     (define title (hash-ref metaobj 'title))
     (parameterize ([self-addr addr]
                    [toc/depth (if (hash-ref metaobj 'toc/depth) (hash-ref metaobj 'toc/depth) 2)]
                    [generate-root? (root? addr)])
-    (output-xml
-      (list
+      (output-xml
+       (list
         (doctype 'html)
-        (if (root? addr)
-          (common-share #:title title
-            (div 'class: "top-wrapper"
-              (tree (build-path "_tmp" (string-append addr ".embed.html")))))
-          (common-share #:title title
-            (div 'class: "top-wrapper"
-              (main (tree (build-path "_tmp" (string-append addr ".embed.html"))))
-            (generate-toc))
-            (footer
-              (generate-context)
-              (generate-references)
-              (generate-backlinks)
-              (generate-related))))
-        )
-      out))
+        (cond
+          [(root? addr)
+           (define fedi (get-config 'fedi #f))
+           (common-share #:title title
+                         #:fedi-validation (if fedi
+                                               (link 'rel: "me"
+                                                     'href: (format "https://~a/@~a" (hash-ref fedi 'site) (hash-ref fedi 'handle))
+                                                     "Mastodon")
+                                               #f)
+                         (div 'class: "top-wrapper"
+                              (tree (build-path "_tmp" (string-append addr ".embed.html")))))]
+          [else
+           (define fedi (get-config 'fedi #f))
+           (common-share #:title title
+                         #:fedi-signature (if fedi
+                                              (meta 'name: "fediverse:creator"
+                                                    'content: (format "@~a@~a" (hash-ref fedi 'handle) (hash-ref fedi 'site)))
+                                              #f)
+                         (div 'class: "top-wrapper"
+                              (main (tree (build-path "_tmp" (string-append addr ".embed.html"))))
+                              (generate-toc))
+                         (footer
+                          (generate-context)
+                          (generate-references)
+                          (generate-backlinks)
+                          (generate-related)))]))
+       out))
     (close-output-port out)))
 
 (define generate-root? (make-parameter #f))
 
-(define (common-share #:title this-title . content)
+(define (common-share #:title this-title
+                      #:fedi-validation [fedi-validation #f]
+                      #:fedi-signature [fedi-signature #f]
+                      . content)
   (html
    (head
     (meta 'http-equiv: "Content-Type" 'content: "text/html; charset=utf-8")
     (meta 'name: "viewport" 'content: "width=device-width, initial-scale=1")
+    fedi-signature
     (title this-title)
     (link 'rel: "stylesheet" 'href: "/katex.min.css")
     (link 'rel: "stylesheet" 'href: "/style.css")
+    fedi-validation
 
     (script 'src: "/minisearch/index.min.js")
     (script 'src: "/tiny.js"))
    (body 'id: "whole"
-      (dialog 'id: "search-dialog"
-        (input 'type: "text" 'id: "search-bar"
-          'spellcheck: "false" 'autocomplete: "off"
-          'placeholder: "Start typing a note title or ID")
-        (div 'id: "search-result"))
-      (unless (generate-root?)
-        (a 'class: "link-home" 'href: "/" "« Home"))
-      content
-      (script 'src: "/fullTextSearch.js"))))
+         (dialog 'id: "search-dialog"
+                 (input 'type: "text" 'id: "search-bar"
+                        'spellcheck: "false" 'autocomplete: "off"
+                        'placeholder: "Start typing a note title or ID")
+                 (div 'id: "search-result"))
+         (unless (generate-root?)
+           (a 'class: "link-home" 'href: "/" "« Home"))
+         content
+         (script 'src: "/fullTextSearch.js"))))
